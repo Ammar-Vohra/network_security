b1f313cfce37882d9f579491c997fe1d2df0ddae
Ammar Vohra <ammarvohra92@gmail.com>
2025-04-17 22:51:03 +0500

    Data Transformation Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 """
 DEFINING COMMON CONSTANT VARIABLES FOR TRAINING PIPELINE
@@ -10,6 +11,7 @@ FILE_NAME : str = 'phisingData.csv'
 TRAIN_FILE_NAME : str = "train.csv"
 TEST_FILE_NAME : str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+PROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 
 """
@@ -33,3 +35,17 @@ DATA_VALIDATION_VALID_DIR = "valid"
 DATA_VALIDATION_INVALID_DIR = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "report.yaml"
+
+
+"""
+DATA TRANSFORMATION RELATED CONSTANTS STARTING WITH DATA_TRANSFORMATION VAR NAME
+"""
+
+DATA_TRANSFORMATION_DIR_NAME = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR = "transformed_object"
+DATA_TRANSFORMATION_IMPUTER_PARAMS = {
+    "missing_values" : np.nan, 
+    "n_neighbors" : 3, 
+    "weights" : "uniform"
+}
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,6 +1,7 @@
 import os, sys
 import yaml
 import pickle
+import numpy as np
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
@@ -22,3 +23,23 @@ def write_yaml_file(file_path, content, replace=False) -> None:
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+
+
+def save_object(file_path, object) -> None:
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file:
+            pickle.dump(object, file) 
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+    
+
+def save_numpy_array_data(file_path, array) -> None:
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file:
+            np.save(file, array)   # Correct order
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
